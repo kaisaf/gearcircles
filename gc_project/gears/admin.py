eec0c4b95ecd08835cc51aab9c5ea198faec2ff0
Kaisa Filppula <kaisa.filppula@gmail.com>
2015-09-20 23:07:07 -0400

    added models to admin and changed default auth

--- gc_project/gears/admin.py
+++ gc_project/gears/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, CategoryProperty, Gear, GearProperty
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(CategoryProperty)
+admin.site.register(Gear)
+admin.site.register(GearProperty)
