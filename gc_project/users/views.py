4bf2415a96081bb1f7d28d5a12a99dbd2d30391c
Victor Menezes <menezes.victor@gmail.com>
2015-09-22 12:09:22 -0400

    adding gitkit configs

--- gc_project/users/views.py
+++ gc_project/users/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
-# Create your views here.
+
 
 class LoginView(View):
     def get(self, request):
         return HttpResponse("MOI! LoginView")
-
+    
+    
 class LogoutView(View):
     def get(self, request):
         return HttpResponse("Hei! LogoutView")
-
+    
+    
 class LoginWidgetView(View):
     def get(self, request):
-        return HttpResponse("MOI! LoginwidgetView")
-
+        return render(request, 'users/loginwidget.html')
+    
+    
 class MyAccountView(View):
     def get(self, request):
         return HttpResponse("MyAccount!!!!!")
-
+    
+    
 class UserView(View):
     def get(self, request, user_id):
         return HttpResponse("Userview " + user_id)
