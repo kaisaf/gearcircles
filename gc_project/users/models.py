4c7f04df2f97703c832cf4ab24c31a65fd0df953
Kaisa Filppula <kaisa.filppula@gmail.com>
2015-09-24 16:06:11 -0400

    removed location comment

--- gc_project/users/models.py
+++ gc_project/users/models.py
@@ -29,7 +29,6 @@ class User(AbstractBaseUser):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, blank=True)
-    #location =
     score = models.FloatField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
