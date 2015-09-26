1a2ba81881ab5adb346266b23b9d2130ee3bcc28
Kaisa Filppula <kaisa.filppula@gmail.com>
2015-09-26 19:34:32 -0400

    refactoring google maps integration

--- gc_project/seeds.py
+++ gc_project/seeds.py
@@ -30,3 +30,20 @@ category_properties = ['model', 'size', 'length', 'width']
 
 for category in categories:
     Category.objects.create(name=category, description="{} description".format(category))
+
+
+"""
+Coordinates:
+from faker import faker
+from gears.models import Location
+from django.contrib.gis.geos import Point, fromstr
+
+fake = Faker()
+
+for i in range(100):
+    lat = fake.geo_coordinate(center=40.7512969, radius=0.1)
+    lng = fake.geo_coordinate(center=-73.988602, radius=0.1)
+    point = fromstr("POINT({} {})".format(lat, lng)
+    address = str[i] + ". location"
+    Location.objects.create(address=address, point=point)
+"""
--- gc_project/static/js/gmaps.js
+++ gc_project/static/js/gmaps.js
@@ -1,14 +1,66 @@
 $(document).ready(function() {
 
-  function initMap() {
+  function getPosition() {
     // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.828175, lng: -98.5795},
-      scrollwheel: false,
-      zoom: 5
-    });
+    defaultPos = {
+      "coords": {
+        "latitude": 40.7509862,
+        "longitude": -73.986871
+      }
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showMap, showMap(defaultPos));
+    } else {
+      showMap(defaultPos)
+    }
   }
 
-  initMap();
+  function convertCoords(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    return {lat: latitude, lng: longitude}
+  }
+
+  function showMap(position) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: convertCoords(position),
+        scrollwheel: false,
+        zoom: 6
+    });
+    addMArker(position, map)
+  }
+
+  function addMArker(position, map, content) {
+    var marker = new google.maps.Marker({
+        position: convertCoords(position),
+        map: map,
+        title: "I'm here!"
+    });
+
+    if (content) {
+      var contentString = "<h3>MOIIII</h3>"
+
+      var infowindow = new google.maps.InfoWindow({
+       content: contentString
+      });
+
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+    }
+
+  }
+
+
+
+    // var marker = new google.maps.Marker({
+    //   position: {lat: 38.828175, lng: -97.5795},
+    //   icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+    //   map: map,
+    //   title: "I'm here!",
+    // });
+
+
+  getPosition();
 
 })
