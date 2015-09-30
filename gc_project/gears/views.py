b16069f904229afc17a2c2728fe9bc87229ccda4
Victor Menezes <menezes.victor@gmail.com>
2015-09-30 16:27:34 -0400

    Locations endpoints filtering by list of categories

--- gc_project/gears/views.py
+++ gc_project/gears/views.py
@@ -73,43 +73,31 @@ class LocationViewSet(viewsets.ModelViewSet):
     filter_fields = ("address",)
 
     def get_queryset(self):
-        print("QUERY STRING = {}".format(self.request.query_params))
-        print("CATEGORIES WITH GET LIST = {}".format(self.request.query_params.getlist('categories[]')))
-        print("CATEGORIES WITH GET = {}".format(self.request.GET.get('categories[]')))
         query_params = Q()
+        categories_params = Q()
+        
+        categories = self.request.query_params.getlist('categories[]')
         latitude = (self.request.query_params.get('lat'))
         longitude = (self.request.query_params.get('lng'))
         distance = (self.request.query_params.get('miles'))
-        category = (self.request.query_params.get('category'))
-        #price = (self.request.query_params.get('price'))
         gear = (self.request.query_params.get('gear'))
         user = (self.request.query_params.get('user'))
+        
         if latitude and longitude and distance:
             center = fromstr("POINT({} {})".format(latitude, longitude))
             distance_from_point = {'mi': distance}
             query_params.add(Q(point__distance_lte=(center, Distance(**distance_from_point))), query_params.connector)
-
-            query_params.add(Q(address='new york'), query_params.connector)
-        if category:
-            query_params.add(Q(gear__categories__name=category), query_params.connector)
-        # if price:
-        #     query_params.add(Q(gear__categories__name=category), query_params.connector)
+            
+        if categories:
+            for i in categories:
+                categories_params.add(Q(gear__categories__id=i), categories_params.OR)
         if gear:
             query_params.add(Q(gear__id=gear), query_params.connector)
         if user:
             query_params.add(Q(gear__user__id=user), query_params.connector)
-            #for key, val in options.items():
-                #qs.add(Q(attributes__name=key) & Q(attribute_values__value_option__option=val), qs.connector)
-
-            #queryset = self.queryset.filter(
-            #point__distance_lte=(center, Distance(**distance_from_point)),
-            #address="boston",
-            #gear__price=100,
-            #    query_params
-            #).distance(center).order_by('distance')
-        #else:
-            #queryset = self.queryset.filter()
-        queryset = self.queryset.filter(query_params)#.distance(center).order_by('distance')
+        print(query_params)
+        print(categories_params)
+        queryset = self.queryset.filter(query_params).filter(categories_params)#.distance(center).order_by('distance')
         return queryset
 
 
