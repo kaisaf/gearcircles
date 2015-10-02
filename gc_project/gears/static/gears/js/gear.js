1d3419e6c328257b07125aca3f40a7c8b323229c
Kaisa Filppula <kaisa.filppula@gmail.com>
2015-10-02 00:00:14 -0400

    GearView Modal working dynamically and validating input

--- gc_project/gears/static/gears/js/gear.js
+++ gc_project/gears/static/gears/js/gear.js
@@ -6,25 +6,36 @@ $(document).ready(function() {
 
   var startDate = null;
   var endDate = null;
+  var price = $('#totalPrice span').text()
+
+  function splitDate(dateString) {
+    return dateString.split('-');
+  }
 
   $('#startDate').on('change', function() {
     startDate = $(this).val();
-    console.log(startDate);
+    partsStart = splitDate(startDate);
+    $('#endDate').val(startDate);
+    document.getElementById("endDate").min = $(this).val();
+    if (endDate) {
+      startDate = new Date(partsStart[0],partsStart[1],partsStart[2]);
+      endDate = new Date(partsEnd[0],partsEnd[1],partsEnd[2]);
+      var difference = Math.round((endDate - startDate)/(1000*60*60*24));
+      totalPrice = (difference + 1)*price;
+      $('#totalPrice span').text(totalPrice);
+    }
   })
 
   $('#endDate').on('change', function() {
     endDate = $(this).val();
+    partsEnd = splitDate(endDate);
     if (startDate) {
-      var partsStart = startDate.split('-');
-      var partsEnd = endDate.split('-');
       startDate = new Date(partsStart[0],partsStart[1],partsStart[2]);
       endDate = new Date(partsEnd[0],partsEnd[1],partsEnd[2]);
-      var difference = Math.Abs(endDate - startDate);
-      console.log(difference)
-      $('#totalPrice').text("PRICE: " + difference);
+      var difference = Math.round((endDate - startDate)/(1000*60*60*24));
+      totalPrice = (difference + 1)*price;
+      $('#totalPrice span').text(totalPrice);
     }
   })
-
-
 
 })
