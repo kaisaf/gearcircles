8ba231a4273c04f93e8162227637dbcda8732285
Kaisa Filppula <kaisa.filppula@gmail.com>
2015-10-03 16:47:49 -0400

    added input types and required for categoryproperties

--- gc_project/gears/static/gears/js/addgear.js
+++ gc_project/gears/static/gears/js/addgear.js
@@ -20,11 +20,30 @@ function createFormElements(categoryProperties) {
     console.log(item)
     var htmlInput = '<div class="form-group"> \
       <label for="exampleInputEmail1">'+item.name+'</label> \
-      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="'+item.description+'"> \
+      <input ' + convertInputTypeAttr(item.input_type) +
+      ' class="form-control" id="exampleInputEmail1" placeholder="'+
+      item.description +'" ' + convertInputRequiredAttr(item.mandatory) +'> \
     </div>'
 
     $("#frm-elements-container").append(htmlInput);
   })
+}
+
+function convertInputTypeAttr(input_type) {
+  if (input_type==0) {
+    return 'type="text"';
+  } else if (input_type==1) {
+    return 'type="number" step="1"';
+  } else if (input_type==2) {
+    return 'type="number" step="0.1"';
+  }
+}
+
+function convertInputRequiredAttr(mandatory) {
+  if (mandatory) {
+    return "required";
+  }
+  return ''
 }
 
 })
