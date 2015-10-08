05958a3d552bb0e45823998a419f4cfe6f797d47
Kaisa Filppula <kaisa.filppula@gmail.com>
2015-10-08 15:13:05 -0400

    reorganized admin

--- gc_project/gears/admin.py
+++ gc_project/gears/admin.py
@@ -2,18 +2,57 @@ from django.contrib.gis import admin
 from .models import Category, CategoryProperty, Location, Gear, GearProperty, GearAvailability, GearImage
 
 
-#class CategoryPropertyInline(admin.StackedInline):
-#    model = CategoryProperty.categories.through
-#    extra = 3
-#
-#class CategoryAdmin(admin.ModelAdmin):
-#    inlines = [CategoryPropertyInline]
+class CategoryPropertyInline(admin.StackedInline):
+    model = CategoryProperty
+    extra = 1
 
 
-admin.site.register(Category)
-admin.site.register(CategoryProperty)
-admin.site.register(Gear)
-admin.site.register(GearProperty)
-admin.site.register(GearAvailability)
+class GearPropertyInline(admin.TabularInline):
+    model = GearProperty
+    extra = 1
+
+
+class GearAvailabilityInline(admin.StackedInline):
+    model = GearAvailability
+    extra = 1
+
+
+class GearImageInline(admin.StackedInline):
+    model = GearImage
+    extra = 1
+
+
+class LocationInline(admin.StackedInline):
+    model = Location
+    extra = 1
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =  ('name', 'description')
+    search_fields = ['name']
+    inlines = [CategoryPropertyInline]
+
+
+class CategoryPropertyAdmin(admin.ModelAdmin):
+    list_display =  ('name', 'category', 'description')
+
+
+class GearAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'user', 'price', 'location')
+    search_fields = ['name']
+    list_filter = ['category']
+    inlines = [GearPropertyInline, GearAvailabilityInline, GearImageInline]
+
+
+class GearPropertyAdmin(admin.ModelAdmin):
+    list_display =  ('value', 'gear')
+
+
+
+
+admin.site.register(Category, CategoryAdmin)
+#admin.site.register(CategoryProperty, CategoryPropertyAdmin)
+admin.site.register(Gear, GearAdmin)
+#admin.site.register(GearProperty, GearPropertyAdmin)
+#admin.site.register(GearAvailability)
 admin.site.register(Location)
-admin.site.register(GearImage)
+#admin.site.register(GearImage)
