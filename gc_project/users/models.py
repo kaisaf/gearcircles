23dc7db5bd6e6d7926b112673e1bf904303b4e7e
Victor Menezes <menezes.victor@gmail.com>
2015-10-13 18:29:10 -0400

    removed name property form User model

--- gc_project/users/models.py
+++ gc_project/users/models.py
@@ -3,30 +3,27 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
-   def create_user(self, email, name, phone=None, score=None, password=None):
+   def create_user(self, email, phone=None, score=None, password=None):
        if not email:
            raise ValueError('Users must have an email address')
 
        user = self.model(
            email=self.normalize_email(email),
-           name=name
        )
 
        user.set_password(password)
        user.save(using=self._db)
        return user
 
-   def create_superuser(self, email, password, name, phone=None, score=None):
+   def create_superuser(self, email, password, phone=None, score=None):
        user = self.create_user(email,
            password=password,
-           name=name
        )
        user.is_admin = True
        user.save(using=self._db)
        return user
 
 class User(AbstractBaseUser):
-    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, blank=True)
     score = models.FloatField(blank=True, null=True)
@@ -38,31 +35,30 @@ class User(AbstractBaseUser):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'phone', 'score']
+    REQUIRED_FIELDS = ['phone', 'score']
 
     def get_full_name(self):
-       # The user is identified by their email address
+       """The user is identified by their email address"""
        return self.email
 
     def get_short_name(self):
-       # The user is identified by their email address
+       """The user is identified by their email address"""
        return self.email
 
     def __str__(self):              # __unicode__ on Python 2
-       return self.email
+       return "ID: {} EMAIL: {}".format(self.id, self.email)
 
     def has_perm(self, perm, obj=None):
-       "Does the user have a specific permission?"
+       """Does the user have a specific permission?"""
        # Simplest possible answer: Yes, always
        return True
 
     def has_module_perms(self, app_label):
-       "Does the user have permissions to view the app `app_label`?"
+       """Does the user have permissions to view the app `app_label`?"""
        # Simplest possible answer: Yes, always
        return True
 
     @property
     def is_staff(self):
-       "Is the user a member of staff?"
-       # Simplest possible answer: All admins are staff
+       """Is the user a member of staff?"""
        return self.is_admin
