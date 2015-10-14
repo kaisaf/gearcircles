22cec5e9d1fc8bd80256604df20796afaebecaa0
Victor Menezes <menezes.victor@gmail.com>
2015-10-13 20:13:32 -0400

    removed 'name' from tests

--- gc_project/users/tests.py
+++ gc_project/users/tests.py
@@ -3,9 +3,9 @@ from users.models import User
 
 class UserTestCase(TestCase):
     def setUp(self):
-        self.kaisa = User(name="Kaisa", email="kaisa@kaisa.com", phone="345-435-344", score=10)
+        self.kaisa = User(email="kaisa@kaisa.com", phone="345-435-344", score=10)
         self.kaisa.save()
-        User.objects.create(name="Victor", email="victor@victor.com", phone="3443-343", score=9.75)
+        User.objects.create(email="victor@victor.com", phone="3443-343", score=9.75)
 
     def test_query_user(self):
         self.assertEqual(len(User.objects.all()), 2)
